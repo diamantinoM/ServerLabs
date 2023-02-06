a4509139b8c0895c9cf484f49361ffb30d475a31
Diamantino <filipemoura869@gmail.com>
2023-02-06 11:53:58 +0000

    Finished Produto and add CatalogoPordutos

--- python/gestao_produtos.py
+++ python/gestao_produtos.py
@@ -62,17 +62,85 @@ class Produto:
         self.quantidade = quantidade
         self.preco = preco
 
-    def __str__(self):
+
+    @property
+    def desc_tipo(self) -> str:
+        return PRODUCT_TYPES[self.tipo]
+
+
+    def __str__(self) -> str:
         cls_name = self.__class__.__name__
-        return f'{cls_name}[id_={self.id}  nome= "{self.nome}" tipo = "{self.tipo}"]'
+        return f'{cls_name}[id_={self.id_}  nome= "{self.nome}" tipo = "{self.tipo}"]'
+
+    def __repr__(self) -> str:
+        cls_name = self.__class__.__name__
+        return f'{cls_name}(id_={self.id_}, nome={self.nome}, tipo={self.tipo}, quantidade={self.quantidade}, preco={repr(self.preco)})'
+
+    def com_iva(self, taxa_iva: dec) -> dec:
+        return self.preco * (1 + taxa_iva/100)
+
+
 
 class InvalidProdAttribute(ValueError):
     pass
 
-def main() -> None:
-    prod1 = Produto(30987, 'pão de milho', 'AL', 2, dec('1'))
 
-    print(f'prod1: {prod1.nome}')
+
+class CatalogoProdutos:
+    def __init__(self):
+        self._prods = {}
+
+
+    def append(self, prod: Produto):
+        if prod.id_ in self._prods:
+            raise DuplicateValue(f'Já existe produto com id {prod.id_} no catálogo')
+        self._prods[prod.id_] = prod
+
+
+    def _dump(self):
+        for prod in self._prods.values():
+            print(prod)
+
+
+    def obtem_por_id(self, id: int) -> Produto | None:
+        return self._prods.get(id)
+
+
+    def pesquisa(self, criterio) -> 'CatalogoProdutos':
+        encontrados = CatalogoProdutos()
+        for prod in self._prods.values():
+            if criterio(prod):
+                encontrados.append(prod)
+        return encontrados
+
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return f'{class_name}[#produtos = {len(self._prods)}]'
+
+
+    def __iter__(self):
+        for prod in self._prods.values():
+            yield prod
+
+
+    def __len__(self):
+        return len(self._prods)
+
+
+
+class DuplicateValue(Exception):
+    pass
+
+
+def main() -> None:
+    produtos = CatalogoProdutos()
+    produtos.append(Produto(30987, 'pão de milho', 'AL', 2, dec('1')))
+    produtos.append(Produto(30098, 'leite mimosa', 'AL', 10, dec('2')))
+    produtos.append(Produto(21109, 'fairy', 'DL', 20, dec('3')))
+    # produtos.append(Produto(21109, 'fairy', 'DL', 20, dec('3')))
+    
+    produtos._dump()
 
 
 if __name__ == '__main__':
