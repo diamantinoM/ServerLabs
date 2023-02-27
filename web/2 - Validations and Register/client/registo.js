302caa75cce7e054e0ac6c126d0bfc2663e810c0
Diamantino <filipemoura869@gmail.com>
2023-02-27 09:07:24 +0000

    developted backend and frontend

---
+++ web/2 - Validations and Register/client/registo.js
@@ -0,0 +1,109 @@
+import { 
+    installValidators,
+    validateAllFields,
+    resetAllFields,
+    addPredicates,
+} from './validations.js';
+
+import {
+    bySel,
+    whenClick,
+    syncWait,
+    isValidDate,
+    byPOSTasJSON,
+} from './utils.js';
+
+
+const URL = 'http://127.0.0.1:8000';
+const TOURNAMENT_ID = 1;
+
+addPredicates({
+    fullName           : /^\p{Letter}{2,}( \p{Letter}{2,})+$/u,
+    password           : { 
+        expr: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#$%&]).{6,10}$/,
+        showSuccess: true,
+    },
+    confirmPassword     : {
+        expr: confirmPassword,
+        showSuccess: true,
+    },
+    'date_DD/MM/YYYY'  : isValidDate,
+    phoneNumber        : /^(\+\d{3})?\d{9}$/,
+});
+
+window.addEventListener('load', function() {
+    installValidators();
+    whenClick('reset', e => resetAllFields());
+    whenClick('submit', validateAndSubmitForm);
+});
+
+async function validateAndSubmitForm() {
+    if (!validateAllFields()) {
+        return
+    }
+    try {
+        const [responseOK, responseData] = await registerPlayer();
+        const showStatusFn = responseOK ? showSuccess : showError;
+        showStatusFn(responseData);
+    }
+    catch (error) {
+        console.error(`ERROR: An error has ocurred connecting to server at ${URL}`)
+        console.error(error)
+    }
+}
+
+async function registerPlayer() {
+    const player = {
+        full_name     : bySel('[name=fullName]').value,
+        email         : bySel('[name=email]').value,
+        password      : bySel('[name=password]').value,
+        phone_number  : bySel('[name=phoneNumber]').value,
+        birth_date    : bySel('[name=birthDate]').value,
+        level         : bySel('[name=level]').value,
+        tournament_id : TOURNAMENT_ID,
+    };
+    const response = await byPOSTasJSON(`${URL}/register`, player);
+    return [response.ok, await response.json()];
+}
+
+
+/**
+ * @param {Object} responseData 
+ */
+function showSuccess(responseData) {
+    const msg = `Inscrição realizada com sucesso.<br>
+Player: ${responseData.full_name} <br>
+ID: ${responseData.id} <br>
+Email: ${responseData.email}`;
+    const formFields = document.querySelector('.info');
+    formFields.style.display = 'none';
+    const elemsToHide = document.querySelectorAll('form > button, .checkbox');
+    elemsToHide.forEach(elem => elem.style.display = 'none');
+    showSubmissionInfo(msg, true);
+}
+
+/**
+ * @param {Object} responseData 
+ */
+function showError(responseData) {
+    const errorInfo = responseData.detail;
+    const msg = `Não foi possível concluir a inscrição. ${errorInfo.error_msg}`;
+    showSubmissionInfo(msg, false);
+}
+
+function showSubmissionInfo(msg, success) {
+    const submissionStatusElem = document.querySelector('.submission-status');
+    const [cssClassToAdd, cssClassToRem] = (success ? 
+          ['submission-status-ok', 'submission-status-error'] 
+        : ['submission-status-error', 'submission-status-ok'] 
+    );
+    submissionStatusElem.innerHTML = `${msg}`;
+    submissionStatusElem.classList.add(cssClassToAdd);
+    submissionStatusElem.classList.remove(cssClassToRem);
+}
+
+function confirmPassword(passwd2) {
+    const passwd1 = document.getElementById('password').value;
+    return passwd1 && passwd1 === passwd2;
+}
+
---
+++ web/2 - Validations and Register/server/schemas.py
@@ -0,0 +1,62 @@
+"""
+In this module we define the Pydantic models. While these models are
+used to interface FastAPI with http clients, SQLAlchemy models 
+are used to talk to the database.
+
+NOTE: There will some code duplication because some attributes will
+have to be defined twice, once for the Pydantic models, and once for
+SQLAlchemy. In another coming example, we will introduce a new library
+that will bridge the two libraries, and prevent some of the code
+duplication that happens when you use Pydantic and SQLAlchemy
+separately.
+
+We will create an PlayerBase Pydantic models (or let's say "schemas") 
+to have common attributes while creating or reading data.
+
+And create an PlayerRegister that inherit from it (so they will have 
+the same attributes), plus any additional data (attributes) needed for
+creation.
+
+So, the user will also have a password when creating it. But for 
+security, the password won't be in other Pydantic models, for example,
+it won't be sent from the API when reading a user.
+"""
+
+
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class PlayerBase(BaseModel):
+    full_name: str
+    email: str
+
+#
+# Nota stor
+#
+
+class PlayerRegister(PlayerBase):
+    password: str | None = Field(
+        title="Local portuguese phone number or international prefixed w/ +XYZ country code"
+    )
+    birth_date: str | None
+    level: str
+    tournament_id: int | None
+
+
+class PlayerRegisterResult(PlayerBase):
+    id: int
+
+
+class ErrorCode(Enum):
+    ERR_UNSPECIFIED_TOURNAMENT = "Missing tournament id."
+    ERR_PLAYER_ALREADY_ENROLLED = "Player already enrolled in tournament."
+    ERR_UNKNOWN_TOURNAMENT_ID = "Unkown tournament id."
+
+
+    def details(self, **kargs) -> dict:
+        details_dict = {'error_code': self.name, 'error_msg': self.value}
+        details_dict.update(kargs)
+        return details_dict
+       
